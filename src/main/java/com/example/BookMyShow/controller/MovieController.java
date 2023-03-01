d4c20fb4b0589ab92f5db51646c895404db3ad34
varis1807 <vrvarisrana123@gmail.com>
2023-03-01 18:44:08 +0530

    commit 2

--- src/main/java/com/example/BookMyShow/controller/MovieController.java
+++ src/main/java/com/example/BookMyShow/controller/MovieController.java
@@ -18,6 +18,7 @@ public class MovieController {
     MovieServiceImpl movieService;
 
 
+    //
     @PostMapping("/add")
     public MovieResponseDto addMovie(@RequestBody() MovieEntryDto movieEntryDto){
 
